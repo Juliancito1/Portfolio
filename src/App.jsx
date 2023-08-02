494de2a56bbc990e804a2d83b32a6c904a4c77a1
Julian <julianser@hotmail.com.ar>
2023-08-02 03:33:56 -0300

    Se agrego el componente redirigir y se utilizaran  botones para mandar a partes especificas del portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import SobreMi from './components/SobreMi'
-import { Row, Col } from 'react-bootstrap';
 function App() {
 
   return (
-    <section>
-      <Row>
-        <Col md={6} className='p-0'>
+    <section className='mainSection'>
         <SobreMi/>
-        </Col>
-      </Row>
     </section>
   )
 }
