c1388eca4b44cfc34d833f5498295fdade6be9bd
Julian <julianser@hotmail.com.ar>
2023-08-02 03:01:15 -0300

    Se agrego una imagen y una breve descripcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import SobreMi from './components/SobreMi'
+import { Row, Col } from 'react-bootstrap';
 function App() {
 
   return (
-    <section>     
-      <SobreMi/>
+    <section>
+      <Row>
+        <Col md={6} className='p-0'>
+        <SobreMi/>
+        </Col>
+      </Row>
     </section>
   )
 }
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
+import React from "react";
+import { Container } from "react-bootstrap";
+import Imagen_CV from "../assets/Imagen_CV.jpeg";
 const SobreMi = () => {
-    return (
-        <section className='mainSection'>
-            <Container>
-                <h1>XD</h1>
-            </Container>
+  return (
+    <section className="mainSection text-light">
+      <Container>
+        <div className="text-center">
+          <img src={Imagen_CV} className="rounded-circle w-25"></img>
+        </div>
+        <h1 className="display-1 text-center">Julian Martin</h1>
+        <section className="container">
+          <h2>Sobre Mi</h2>
+          <p className="fs-5">
+            Soy un chico, que busca mejorar sus conocimientos en la programación
+            y superarse en el día a día. Dando lo mejor de mi y logrando hacer
+            un gran trabajo en cada proyecto que me proponga
+          </p>
         </section>
-    );
+      </Container>
+    </section>
+  );
 };
 
 export default SobreMi;
