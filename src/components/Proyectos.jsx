d80ecfd87f72fe22aff66282d64b469cb8fd6909
Julian <julianser@hotmail.com.ar>
2023-08-07 20:34:57 -0300

    Se hicieron cambios en la seccion proyectos y en el navbar

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 import proyectobondia from "../assets/Proyecto.png"
+import proyecto2 from "../assets/Proyecto2.png"
 const Proyectos = () => {
     return (
         <Container className='text-center text-light'>
             <h2 className='display-3'>Proyectos</h2>
             <hr></hr>
             <Row>
-            <Col md={6}>
-            <Card>
+            <Col md={12}>
+            <Card bg='secondary'>
                 <Card.Header>
-                    <img className='img-fluid' src={proyectobondia} alt="" />
+                    <Card.Title>
+                        <img className='img-fluid' src={proyecto2} alt="" />
+                        </Card.Title>
                 </Card.Header>
                 <Card.Body>
-                    Este fue un proyecto realizado de forma grupal
+                    <Card.Text>
+                    <h2 className='text-light'>Tucumánime proyecto sobre sitio web basado en la venta de productos de ánime, realizado de forma grupal</h2>
+                    </Card.Text>
                 </Card.Body>
                 <Card.Footer className='d-flex justify-content-end'>
-                    <Button variant='dark' className='fs-3'>Ver proyecto</Button>
+                    <Button href='https://tucumananimeprototipo.netlify.app/' variant='dark' className='fs-3'>Ver proyecto</Button>
+                </Card.Footer>
+            </Card>
+            </Col>
+            <Col md={12}>
+            <Card bg='secondary' className='my-5'>
+                <Card.Header>
+                    <Card.Title>
+                    <img className='img-fluid' src={proyectobondia} alt="" />
+                    </Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                    <h2 className='text-light'>Bon Dia fue un sitio web creado de forma grupal, el mismo se basa en un sistema de pedidos de un restaurante</h2>
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className='d-flex justify-content-end'>
+                    <Button href='https://bondia-restaurant.netlify.app/'variant='dark' className='fs-3'>Ver proyecto</Button>
                 </Card.Footer>
             </Card>
             </Col>
--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,17 +1,19 @@
-import { Navbar, Nav, Container  } from "react-bootstrap";
-
+import { Navbar, Nav, Container, Button  } from "react-bootstrap";
+import logo from "../assets/Logo.png"
 const Menu = () => {
     return (
-        <Navbar expand="lg" className="bg-body-secondary" fixed="top">
-      <Container>
-        <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
+        <Navbar expand="lg" className="pb-2" id="navbar" fixed="top">
+      <Container fluid >
+        <Navbar.Brand href="#home">
+          <img className="logo" src={logo}></img>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#sobremi">Sobre Mi</Nav.Link>
-            <Nav.Link href="#conocimientos">Conocimientos</Nav.Link>
-            <Nav.Link href="#proyectos">Proyectos</Nav.Link>
-            <Nav.Link href="#link">Contacto</Nav.Link>
+          <Nav className="ms-auto text-center">
+            <Nav.Link href="#sobremi"><Button variant="dark" className="fs-3 w-100" id="boton-navbar">Sobre Mi</Button></Nav.Link>
+            <Nav.Link href="#conocimientos"><Button variant="dark" className="fs-3 w-100" id="boton-navbar">Conocimientos</Button></Nav.Link>
+            <Nav.Link href="#proyectos"><Button variant="dark" className="fs-3 w-100" id="boton-navbar">Proyectos</Button></Nav.Link>
+            <Nav.Link href="#link"><Button variant="dark" className="fs-3 w-100" id="boton-navbar">Contacto</Button></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
